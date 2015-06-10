960f9219ad24bf91fe586035bde6f296aba85ea3
Kristen Rieger <karieger1@gmail.com>
2015-06-09 22:59:47 -0500

    mostly done, except styling

--- dist/scripts/collections/image-collection.js
+++ dist/scripts/collections/image-collection.js
@@ -1,5 +1,7 @@
 var ImageCollection = Backbone.Collection.extend ({
 	model: image,
-	url: "http://tiy-fee-rest.herokuapp.com/collections/here-you-go-aaron"
+	//i know i'm supposed to have my own url here, but i don't know how 
+	//to do this on tiny pizza server
+	url: "http://tiy-fee-rest.herokuapp.com/collections/here-you-go-aaron",
 	idAtrribute: "_id"
 });
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
 
 	var imageList = new ImageCollection();
+		imageList.fetch();
 
 	var imageBuilder = _.template($("#image-template").html());
 
 	$("#imageForm").submit(function(e) {
 	        e.preventDefault();
-	        console.log('poop');
+	        console.log("poop");
+	        e.preventDefault();
 	});
+
+	var newImage = new image({
+			imageUrl: $("#imageURL").val(),
+			caption: $("#imageCaption").val()
+		});
+
+	if(newImage.isValid()) {
+			imageList.add(newImage);
+			newImage.save();
+			$("#imageURL").val("");
+			$("#imageCaption").val("");
+		
+		} else {
+
+			console.log(newImage.validationError);
+		}
+			console.log(newImage);
+
+		
+	// });
 
 });
